71dd216e9d46ae11523fc869a632761cb27faecc
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-11 16:58:25 +0800

    finish class work but not clear all

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,7 +2,10 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
@@ -14,15 +17,19 @@ public class Add {
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+//        return arrayList.stream().mapToInt(i -> i * 3 + 2).sum();
+        return arrayList.stream().reduce(0,(pre,cur) ->pre+cur*3+2);
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+//        List<Integer> list = new ArrayList<>();
+//        for(int i : arrayList) list.add(i % 2 == 0 ? i : i * 3 + 2);
+//        return list;
+        return arrayList.stream().map((item) -> item % 2 == 0 ? item : item * 3 + 2 ).collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(i->i % 2 != 0).mapToInt(i->i*3+5).sum();
     }
 
     public double getMedianOfEven(List<Integer> arrayList) {
@@ -30,7 +37,7 @@ public class Add {
     }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(i->i % 2 == 0).mapToDouble(i->i).average().orElse(0);
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
